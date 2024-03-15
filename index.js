da631e63267205ee309ad27b601983de6b6eb97a
iannjau <lantanacreations@gmail.com>
2024-03-15 10:22:29 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) {
+    return string.toUpperCase();
+    "Hello".toUpperCase(); // `HELLO!`
+}
+function whisper(string){
+    return string.toLowerCase();
+    `HELLO`.toLowerCase(); // `Hello`
+}
+function logShout(string){
+    console.log.apply();toUpperCase();
+}
